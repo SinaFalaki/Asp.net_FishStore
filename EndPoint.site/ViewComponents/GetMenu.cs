ab5b0b5a77f9f96c1c29987b71ed6f6041eeb18d
Sina Falaki | سینا فلکی <123977796+SinaFalaki@users.noreply.github.com>
2023-05-08 15:29:53 +0330

    add view component

---
+++ EndPoint.site/ViewComponents/GetMenu.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sina_Store.Application.Services.Common.Queries.GetMenuItem;
+
+namespace EndPoint.Site.ViewComponents
+{
+    public class GetMenu : ViewComponent
+    {
+        private readonly IGetMenuItemService _getMenuItemService;
+        public GetMenu(IGetMenuItemService getMenuItemService)
+        {
+            _getMenuItemService = getMenuItemService;
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            var menuItem = _getMenuItemService.Execute();
+            return View(viewName: "GetMenu", menuItem.Data);
+        }
+
+    }
+}
---
+++ EndPoint.site/ViewComponents/Search.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sina_Store.Application.Services.Common.Queries.GetCategory;
+
+namespace EndPoint.Site.ViewComponents
+{
+    public class Search : ViewComponent
+    {
+        private readonly IGetCategoryService _getCategoryService;
+        public Search(IGetCategoryService getCategoryService)
+        {
+            _getCategoryService = getCategoryService;
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            return View(viewName: "Search", _getCategoryService.Execute().Data);
+        }
+    }
+}
