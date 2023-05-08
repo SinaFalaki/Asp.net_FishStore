126babbfda77a942c955ea2503a570a24d06e476
Sina Falaki | سینا فلکی <123977796+SinaFalaki@users.noreply.github.com>
2023-05-08 15:17:23 +0330

    add Common layer

---
+++ Sina-Store.Common/Roles/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace Sina_Store.Common.Roles
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Operator = "Operator";
+        public const string Customer = "Customer";
+    }
+}
---
+++ Sina-Store.Common/pagination.cs
@@ -0,0 +1,11 @@
+﻿namespace Sina_Store.Common
+{
+    public static class pagination
+    {
+        public static IEnumerable<TSource> ToPaged<TSource>(this IEnumerable<TSource> source, int page, int pageSize, out int rowsCount)
+        {
+            rowsCount = source.Count();
+            return source.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
